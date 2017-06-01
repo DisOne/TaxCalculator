316ed8c48a76beb11fc20e731cf43488da502f5c
Hendricus Disseldorp <hdisseldorp@gmail.com>
2017-06-01 13:14:00 +0300

    Initial commit

---
+++ Tax Calculator/Managers/Data Model Controller/DataModelController.swift
@@ -0,0 +1,19 @@
+//
+//  DataModelController.swift
+//  Tax Calculator
+//
+//  Created by Harry Disseldorp on 6/1/17.
+//  Copyright © 2017 DisOne. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DataModelController {
+  // MARK: - Core Data stack
+  
+  private init() {
+    // Make initialization private. Not accessible by others.
+  }
+  
+}
