45eff9d07493b7be9837d86b50ad6b71f39de806
Hendricus Disseldorp <hdisseldorp@gmail.com>
2017-06-03 22:45:18 +0300

    Model and layout changes

---
+++ Tax Calculator/Managers/Data Model Entities/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Tax Calculator
+//
+//  Created by Harry Disseldorp on 6/2/17.
+//  Copyright © 2017 DisOne. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class Settings: NSManagedObject {
+
+}
